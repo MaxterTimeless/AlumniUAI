4ab8c98663f36df9daafe0ff5b52c8fc349171aa
jpvm33jp <80127431+jpvm33jp@users.noreply.github.com>
2022-05-09 20:09:57 -0400

    horario buses-deportes terminados
    se termino los contenedores para los horarios de buses y deportes

--- client/src/App.js
+++ client/src/App.js
@@ -40,13 +40,19 @@ function App() {
 
 
  <div class="container">
- <img src='https://ingenieria.uai.cl/assets/uploads/2020/03/dsc_0062-320x260-c-default@2x.jpg'></img>
+ <img src='https://i.imgur.com/fjFJ6IH.png'></img>
+ <span> Horario de buses</span>
  <a href="https://www.uai.cl/campus-uai/campus-penalolen/buses-de-acercamiento-penalolen/" class="btn">Santiago</a>
  <a href="https://www.uai.cl/campus-uai/campus-vina-del-mar/buses-de-acercamiento-vina-del-mar/" class="bn">Viña del mar</a>
-  <div class="top-left">Horario de buses</div>
-
 </div>
 
+
+<div class="contenedor">
+ <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0DnJWmBeb0MyDg4gS7Fo2V7hNShYAZcZskg&usqp=CAU'></img>
+ <span> Horario de deportes</span>
+ <a href="https://alumno.uai.cl/santiago/deportes/" class="dnt">Santiago</a>
+ <a href="https://alumno.uai.cl/vina-del-mar/deportes/" class="dn">Viña del mar</a>
+</div>
     </Fragment>
   );
 }
