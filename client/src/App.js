42444f49d6d6f89ee24d3d99d8ccf6a7b97c4a26
Maximiliano Navarrete Ulloa <maxnavarrete@alumnos.uai.cl>
2022-05-17 13:11:21 -0400

    Top-menu-2 y ajustes
    -Se agregaron las entradas que faltaban en el menú de la barra superior, incluyendo un sub-menú de Centros y la barra de búsqueda.
    -Se ajustó el padding de la barra superior y footer para no exceder el ancho de la página.

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,11 @@ import React, { Fragment } from "react"
 import './App.css';
 
 function App() {
-  const TopMenu = () => {
-    if(document.getElementById("Dropdown-menu").style.display == "block"){
-      document.getElementById("Dropdown-menu").style.display = "none";
+  const Menu = (dis) => {
+    if(document.getElementById(dis).style.display == "block"){
+      document.getElementById(dis).style.display = "none";
     } else {
-      document.getElementById("Dropdown-menu").style.display = "block";
+      document.getElementById(dis).style.display = "block";
     }
   };
   return (
@@ -18,8 +18,7 @@ function App() {
                 
                 <a href="https://alumnosfic.uai.cl/" class="Titulo">AlumnosFIC</a>
                 
-              
-                <button onClick={TopMenu} class="navicon_js-navicon">
+                <button onClick={()=>Menu("Dropdown-menu")} class="navicon_js-navicon">
                   <svg height="40" viewBox="0 0 24 24" width="40" xmlns="http://www.w3.org/2000/svg">
                     <path d="M2 15.5v2h20v-2H2zm0-5v2h20v-2H2zm0-5v2h20v-2H2z" class="three_lines"/>
                     <path d="M0 0h24v24H0z" fill="none"/>
@@ -28,35 +27,52 @@ function App() {
               </div>
             </div>
             <div id="Dropdown-menu" class="dropdown-content">
-              <a href="#">Información Académica</a>
-              <a href="#">Mallas Curriculares</a>
-              <a href="#">Pregrado</a>
-              <a href="#">Quinto Año</a>
-              <a href="#">Prácticas</a>
-              <a href="#">Mecanismos de Titulación</a>
-              <a href="#">Doble Titulación</a>
-              <a href="#">Internacionalzación</a>
-              <a href="#">Acreditaciones</a>
-              <a href="#">Asuntos Estudiantiles</a>
+              <div class="dropdown-content-1">
+                <a href="#">Información Académica</a>
+                <a href="#">Mallas Curriculares</a>
+                <a href="#">Pregrado</a>
+                <a href="#">Quinto Año</a>
+                <a href="#">Prácticas</a>
+                <a href="#">Mecanismos de Titulación</a>
+                <a href="#">Doble Titulación</a>
+                <a href="#">Internacionalzación</a>
+                <a href="#">Acreditaciones</a>
+                <a href="#">Asuntos Estudiantiles</a>
+              </div> 
+              <div class="dropdown-content-2">
+                <a href="#">Facultad de Ingeniería y Ciencias</a>
+                <a href="#">Biblioteca</a>
+                <p class="Centros">
+                  Centros
+                  <button onClick={()=>Menu("Dropdown-centros")} class="downArrow">
+                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24px" height="24px" viewBox="0 0 24 24" transform="rotate(90)">
+                      <path fill="#dcdcdc" d="M6.4,19.3l7.3-7.3L6.4,4.7l2.3-2.3l9.6,9.6l-9.6,9.6L6.4,19.3z"></path>
+                    </svg>
+                  </button>
+                </p>
+                
+              </div>
+              <div id="Dropdown-centros" class="dropdown-content-centros">
+                <a href="#">iUAI Bio Center</a>
+                <a href="#">iUAI Built Center</a>
+                <a href="#">iUAI Complexity Center</a>
+                <a href="#">iUAI Design Engineering Center</a>
+                <a href="#">iUAI Earth Center</a>
+                <a href="#">iUAI Mining Center</a>
+                <a href="#">iUAI Systems Center</a>
+                <a href="#">iUAI Tech Center</a>
+              </div> 
+              <div class="top-search">
+                <form action="https://ingenieria.uai.cl" method="GET" class="search-form">
+                  <input type="search" name="s" class="search-form__field" placeholder="Ingresa tu búsqueda" />
+                  <button type="submit" class="search-form-s"><svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/><path d="M0 0h24v24H0z" fill="none"/></svg></button>
+                </form>                        
+              </div>
             </div>
-            <div class="top-search">
-              <form action="https://ingenieria.uai.cl" method="GET" class="search-form">
-                <input type="search" name="s" class="search-form__field" placeholder="Ingresa tu búsqueda" />
-                <button type="submit" class="search-form__submit"><svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/><path d="M0 0h24v24H0z" fill="none"/></svg></button>
-              </form>                        
-            </div>
-          </div>
-        </div>
-        <div class="header__wrap" >
-          <div class="site-name">
-            <a href="https://ingenieria.uai.cl">Facultad de Ingeniería y Ciencias</a>
-          </div>
-          <div class="logo">
-            <a href="https://www.uai.cl" title="Universidad Adolfo Ibáñez" rel="home-uai"></a>
             
           </div>
         </div>
-
+        
 
 <div>
  <div class="container">
