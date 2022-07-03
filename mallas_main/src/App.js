2e8e555d71bac624548ad0ead1f9405982167f14
vicente9715 <49298692+vicente9715@users.noreply.github.com>
2022-07-03 17:58:03 -0400

    Página de mallas curriculares
    Se creó la página de mallas curriculares, en donde están las diversas carreras y los años a los cuales se les debiese generar su contenido

--- mallas_main/src/App.js
+++ mallas_main/src/App.js
@@ -1,20 +1,406 @@
-
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div class = "general-content">
+      {/*Mallas curriculares de Bio*/}
+      <div class = "Ingenierias">
+        <div class = "text-container">
+          <h3>
+            Ingeniería Civil en Bioingeniería
+          </h3>
+          <hr></hr>
+        </div>
+      </div>
+      {/*Botones para los años de ingresos de Bio*/}
+      <div class = "Año-ingreso">
+        <a class = "Boton-año"> 
+          <div>
+            <h3>
+              Año Ingreso 2022
+            </h3>
+          </div>    
         </a>
-      </header>
+        <a class = "Boton-año"> 
+          <div>
+            <h3>
+              Año Ingreso 2021
+            </h3>
+          </div>    
+        </a>
+        <a class = "Boton-año"> 
+          <div>
+            <h3>
+              Año Ingreso 2020
+            </h3>
+          </div>    
+        </a>
+        <a class = "Boton-año"> 
+          <div>
+            <h3>
+              Año Ingreso 2019
+            </h3>
+          </div>    
+        </a>
+        <a class = "Boton-año"> 
+          <div>
+            <h3>
+              Año Ingreso 2018
+            </h3>
+          </div>    
+        </a>
+      </div>
+
+      {/*Mallas curriculares de Energía y medioambiente*/}
+      <div class = "Ingenierias">
+        <div class = "text-container">
+          <h3>
+            Ingeniería Civil en Energía y Medioambiente
+          </h3>
+          <hr></hr>
+        </div>
+      </div>
+     {/*Botones para los años de ingresos de Energía y Medioambiente*/}
+      <div class = "Año-ingreso">
+        <a class = "Boton-año"> 
+          <div>
+            <h3>
+              Año Ingreso 2022
+            </h3>
+          </div>    
+        </a>
+        <a class = "Boton-año"> 
+          <div>
+            <h3>
+              Año Ingreso 2021
+            </h3>
+          </div>    
+        </a>
+        <a class = "Boton-año"> 
+          <div>
+            <h3>
+              Año Ingreso 2020
+            </h3>
+          </div>    
+        </a>
+        <a class = "Boton-año"> 
+          <div>
+            <h3>
+              Año Ingreso 2019
+            </h3>
+          </div>    
+        </a>
+        <a class = "Boton-año"> 
+          <div>
+            <h3>
+              Año Ingreso 2018
+            </h3>
+          </div>    
+        </a>
+      </div>
+
+      {/*Mallas curriculares de Industrial*/}
+      <div class = "Ingenierias">
+        <div class = "text-container">
+          <h3>
+            Ingeniería Civil Industrial
+          </h3>
+          <hr></hr>
+        </div>
+      </div>
+      {/*Botones para los años de ingresos de Industrial*/}
+      <div class = "Año-ingreso">
+        <a class = "Boton-año"> 
+          <div>
+            <h3>
+              Año Ingreso 2022
+            </h3>
+          </div>    
+        </a>
+        <a class = "Boton-año"> 
+          <div>
+            <h3>
+              Año Ingreso 2021
+            </h3>
+          </div>    
+        </a>
+        <a class = "Boton-año"> 
+          <div>
+            <h3>
+              Año Ingreso 2020
+            </h3>
+          </div>    
+        </a>
+        <a class = "Boton-año"> 
+          <div>
+            <h3>
+              Año Ingreso 2019
+            </h3>
+          </div>    
+        </a>
+        <a class = "Boton-año"> 
+          <div>
+            <h3>
+              Año Ingreso 2018
+            </h3>
+          </div>    
+        </a>
+      </div>
+
+      {/*Mallas curriculares de Informática*/}
+      <div class = "Ingenierias">
+        <div class = "text-container">
+          <h3>
+            Ingeniería Civil Informática
+          </h3>
+          <hr></hr>
+        </div>
+      </div>
+      {/*Botones para los años de ingresos de Informática*/}
+      <div class = "Año-ingreso">
+        <a class = "Boton-año"> 
+          <div>
+            <h3>
+              Año Ingreso 2022
+            </h3>
+          </div>    
+        </a>
+        <a class = "Boton-año"> 
+          <div>
+            <h3>
+              Año Ingreso 2021
+            </h3>
+          </div>    
+        </a>
+        <a class = "Boton-año"> 
+          <div>
+            <h3>
+              Año Ingreso 2020
+            </h3>
+          </div>    
+        </a>
+        <a class = "Boton-año"> 
+          <div>
+            <h3>
+              Año Ingreso 2019
+            </h3>
+          </div>    
+        </a>
+        <a class = "Boton-año"> 
+          <div>
+            <h3>
+              Año Ingreso 2018
+            </h3>
+          </div>    
+        </a>
+      </div>
+
+    {/*Mallas curriculares de Civil*/}
+      <div class = "Ingenierias">
+        <div class = "text-container">
+          <h3>
+            Ingeniería Civil
+          </h3>
+          <hr></hr>
+        </div>
+      </div>
+      {/*Botones para los años de ingresos de Civil*/}
+       <div class = "Año-ingreso">
+        <a class = "Boton-año"> 
+          <div>
+            <h3>
+              Año Ingreso 2022
+            </h3>
+          </div>    
+        </a>
+        <a class = "Boton-año"> 
+          <div>
+            <h3>
+              Año Ingreso 2021
+            </h3>
+          </div>    
+        </a>
+        <a class = "Boton-año"> 
+          <div>
+            <h3>
+              Año Ingreso 2020
+            </h3>
+          </div>    
+        </a>
+        <a class = "Boton-año"> 
+          <div>
+            <h3>
+              Año Ingreso 2019
+            </h3>
+          </div>    
+        </a>
+        <a class = "Boton-año"> 
+          <div>
+            <h3>
+              Año Ingreso 2018
+            </h3>
+          </div>    
+        </a>
+      </div>
+
+      {/*Mallas curriculares de Minería*/}
+      <div class = "Ingenierias">
+        <div class = "text-container">
+          <h3>
+            Ingeniería Civil en Minería
+          </h3>
+          <hr></hr>
+        </div>
+      </div>
+      {/*Botones para los años de ingresos de Minería*/}
+      <div class = "Año-ingreso">
+        <a class = "Boton-año"> 
+          <div>
+            <h3>
+              Año Ingreso 2022
+            </h3>
+          </div>    
+        </a>
+        <a class = "Boton-año"> 
+          <div>
+            <h3>
+              Año Ingreso 2021
+            </h3>
+          </div>    
+        </a>
+        <a class = "Boton-año"> 
+          <div>
+            <h3>
+              Año Ingreso 2020
+            </h3>
+          </div>    
+        </a>
+        <a class = "Boton-año"> 
+          <div>
+            <h3>
+              Año Ingreso 2019
+            </h3>
+          </div>    
+        </a>
+        <a class = "Boton-año"> 
+          <div>
+            <h3>
+              Año Ingreso 2018
+            </h3>
+          </div>    
+        </a>
+      </div>
+
+      {/*Mallas curriculares de Energía*/}
+      <div class = "Ingenierias">
+        <div class = "text-container">
+          <h3>
+            Ingeniería Civil en Energía
+          </h3>
+          <hr></hr>
+        </div>
+      </div>
+      {/*Botones para los años de ingresos de Energía*/}
+      <div class = "Año-ingreso">
+        <a class = "Boton-año"> 
+          <div>
+            <h3>
+              Año Ingreso 2022
+            </h3>
+          </div>    
+        </a>
+        <a class = "Boton-año"> 
+          <div>
+            <h3>
+              Año Ingreso 2021
+            </h3>
+          </div>    
+        </a>
+      </div>
+
+      {/*Mallas curriculares de Mecánica*/}
+      <div class = "Ingenierias">
+        <div class = "text-container">
+          <h3>
+            Ingeniería Civil en Mecánica
+          </h3>
+          <hr></hr>
+        </div>
+      </div>
+      {/*Botones para los años de ingresos de Mecánica*/}
+      <div class = "Año-ingreso">
+        <a class = "Boton-año"> 
+          <div>
+            <h3>
+              Año Ingreso 2022
+            </h3>
+          </div>    
+        </a>
+        <a class = "Boton-año"> 
+          <div>
+            <h3>
+              Año Ingreso 2021
+            </h3>
+          </div>    
+        </a>
+        <a class = "Boton-año"> 
+          <div>
+            <h3>
+              Año Ingreso 2020
+            </h3>
+          </div>    
+        </a>
+        <a class = "Boton-año"> 
+          <div>
+            <h3>
+              Año Ingreso 2019
+            </h3>
+          </div>    
+        </a>
+        <a class = "Boton-año"> 
+          <div>
+            <h3>
+              Año Ingreso 2018
+            </h3>
+          </div>    
+        </a>
+      </div>
+
+      {/*Mallas curriculares de Bachillerato*/}
+      <div class = "Ingenierias">
+        <div class = "text-container">
+          <h3>
+            Bachillerato
+          </h3>
+          <hr></hr>
+        </div>
+      </div>
+       {/*Botones para los años de ingresos del Bachillerato*/}
+      <div class = "Año-ingreso">
+        <a class = "Boton-año"> 
+          <div>
+            <h3>
+              Año Ingreso 2022
+            </h3>
+          </div>    
+        </a>
+        <a class = "Boton-año"> 
+          <div>
+            <h3>
+              Año Ingreso 2021
+            </h3>
+          </div>    
+        </a>
+        <a class = "Boton-año"> 
+          <div>
+            <h3>
+              Año Ingreso 2020
+            </h3>
+          </div>    
+        </a>
+      </div>
+
+
     </div>
   );
 }
